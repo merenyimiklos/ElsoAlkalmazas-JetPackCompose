f4a9240fbf535e9938751e264191a907f9d7e3d6
Merényi Miklós <89610165+merenyimiklos@users.noreply.github.com>
2026-09-19 22:28:05 +0200

    Translate unit test comments to Hungarian

--- app/src/test/java/hu/petrik/elsoalkalmazas/ExampleUnitTest.kt
+++ app/src/test/java/hu/petrik/elsoalkalmazas/ExampleUnitTest.kt
@@ -1,17 +1,26 @@
 package hu.petrik.elsoalkalmazas
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
+/*
+ * Egyszerű helyi unit teszt.
  *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * Ez a teszt a fejlesztőgépen fut, nem szükséges hozzá telefon vagy emulátor.
+ * Az első órán ezt még nem kell részletesen használni, de jó tudni,
+ * hogy a Kotlin függvényeink működése automatikusan is ellenőrizhető.
  */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
+        // Ellenőrizzük, hogy 2 + 2 valóban 4-et ad-e eredményül.
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun addFunction_isCorrect() {
+        // A saját add() függvényünket is egyszerűen tudjuk tesztelni.
+        assertEquals(12, add(5, 7))
     }
 }
